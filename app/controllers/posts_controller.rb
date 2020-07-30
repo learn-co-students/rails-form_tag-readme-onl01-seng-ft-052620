f16a982567ca4b17896232afdde71207abcade42
Maloochow <luyao.mly@gmail.com>
2020-07-30 17:49:20 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,16 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
+  def new
+  end
+
+  def show
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
 end
